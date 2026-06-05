4e79b8d692e0af7599e3c5f49d0fad31c7a4f524
Devanshu Mishra <devmishra2202@gmail.com>
2026-06-05 20:17:58 +0530

    feat: fetch and display products

--- src/pages/ProductListingPage.tsx
+++ src/pages/ProductListingPage.tsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+import type { Product } from "../types/product";
+import { getProducts } from "../api/productApi";
+
 const ProductListingPage = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const fetchProducts = async () => {
+        try {
+            setLoading(true);
+            setError("");
+
+            const data = await getProducts();
+
+            console.log("data", data
+
+            )
+
+            setProducts(data.products);
+        } catch (err) {
+            setError("Failed to fetch products");
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    if (loading) {
+        return <h2>Loading products...</h2>;
+    }
+
+    if (error) {
+        return <h2>{error}</h2>;
+    }
+
     return (
-        <div>
-            <h1>Product Listing Page</h1>
+        <div className="p-6">
+            <h1 className="mb-4 text-3xl font-bold">
+                Product Listing Page
+            </h1>
+
+            <div className="space-y-2">
+                {products.map((product) => (
+                    <p key={product.id}>{product.title}</p>
+                ))}
+            </div>
         </div>
     );
 };
